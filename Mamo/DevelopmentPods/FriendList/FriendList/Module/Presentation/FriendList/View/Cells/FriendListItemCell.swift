77a6f2975a8600f43f1e4cc7d3c3ef7df08cc037
Huy Nguyen <huy.n.pyco@aux.go-jek.com>
2021-06-06 20:21:27 +0700

    update: friend list with vertical items

--- Mamo/DevelopmentPods/FriendList/FriendList/Module/Presentation/FriendList/View/Cells/FriendListItemCell.swift
+++ Mamo/DevelopmentPods/FriendList/FriendList/Module/Presentation/FriendList/View/Cells/FriendListItemCell.swift
@@ -9,17 +9,49 @@ import Foundation
 
 final class FriendListItemCell: ListCell<FriendListItemViewModel> {
     
-    let imageView = UIImageView(image: UIImage(named: "Image"))
-    let nameLabel = UILabel(text: "Title", font: .boldSystemFont(ofSize: 14))
+    private let imageView = UIImageView()
+    private let nameLabel = UILabel(text: "", font: .monospacedSystemFont(ofSize: 14, weight: .light))
     
     override var item: FriendListItemViewModel! {
         didSet {
+            imageView.image = item.publicName.imageWithFirstCharacter()
             nameLabel.text = item.publicName
         }
     }
     
     override func setupViews() {
         super.setupViews()
-        stack(imageView, nameLabel, spacing: 16)
+        
+        hstack(imageView.withSize(CGSize(width: 60, height: 60)),
+               nameLabel,
+               spacing: 16,
+               alignment: UIStackView.Alignment.center).withMargins(.horizontalSides(16))
+        
+        imageView.layer.cornerRadius = 60 / 2
+        imageView.layer.masksToBounds = true
+    }
+}
+
+extension String {
+    
+    func imageWithFirstCharacter() -> UIImage? {
+        let frame = CGRect(x: 0, y: 0, width: 60, height: 60)
+        let nameLabel = UILabel(text: nil,
+                            font: .boldSystemFont(ofSize: 24),
+                            textColor: .white,
+                            textAlignment: .center)
+        nameLabel.frame = frame
+        nameLabel.backgroundColor = .lightGray
+        guard let firstChar = self.first else {
+            return nil
+        }
+        nameLabel.text = String(firstChar).capitalized
+        UIGraphicsBeginImageContext(frame.size)
+        if let currentContext = UIGraphicsGetCurrentContext() {
+            nameLabel.layer.render(in: currentContext)
+            let nameImage = UIGraphicsGetImageFromCurrentImageContext()
+            return nameImage
+        }
+        return nil
     }
 }
