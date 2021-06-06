934ee8bcd8a4e39eab81ad2af0b87e89e4605b05
Huy Nguyen <huy.n.pyco@aux.go-jek.com>
2021-06-06 10:53:19 +0700

    update: example vertical list controller with UIColor

--- Mamo/Mamo/ViewController.swift
+++ Mamo/Mamo/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import FriendList
 
-class ViewController: UIViewController {
+class SimpleListCell: ListCell<UIColor> {
+    override var item: UIColor! { didSet { backgroundColor = item }}
+}
+
+class ViewController: ListController<SimpleListCell, UIColor>, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        items = [.green, .blue, .red, .yellow, .black, .gray, .darkGray, .systemPink]
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 80)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: view.frame.width, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 0, right: 0)
+    }
 }
 
