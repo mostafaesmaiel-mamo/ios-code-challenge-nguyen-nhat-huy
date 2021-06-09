20626a49956da824d2c75f0201d5cc0c71f6257b
Huy Nguyen <huy.ng1012@gmail.com>
2021-06-09 21:56:13 +0700

    add: extension function center super view

--- Mamo/DevelopmentPods/FriendList/FriendList/Module/Extensions/UIView+Layout.swift
+++ Mamo/DevelopmentPods/FriendList/FriendList/Module/Extensions/UIView+Layout.swift
@@ -118,4 +118,23 @@ extension UIView {
         
         return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
     }
+    
+    open func centerInSuperview(size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
 }
